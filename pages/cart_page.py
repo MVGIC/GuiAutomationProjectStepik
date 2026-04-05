04e55d3fbc42508b64b62246c5a922a24cf780e9
Nikolai Mitroshkin <ex3ct@mail.ru>
2026-04-05 12:15:47 +0300

    cart page added

---
+++ pages/cart_page.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from base.base_class import Base
+
+
+class CartPage(Base):
+    """Класс содержащий локаторы и методы для страницы Корзины"""
+
+
+    # Locators
+
+    checkout_button = "//button[@data-test='checkout']"
+
+
+    # Getters
+
+    def get_checkout_button(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.XPATH, self.checkout_button)))
+
+
+    # Actions
+
+    def click_checkout_button(self):
+        self.get_checkout_button().click()
+        print("Click checkout button")
+
+
+    # Methods (Steps)
+
+    def move_to_checkout(self):
+        """Переход на страницу подтверждения товара"""
+        self.get_current_url()
+        self.click_checkout_button()
